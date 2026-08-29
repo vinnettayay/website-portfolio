ec183c88f0ed84e58e46d841dde2b51d23182671
vinnettayay <fanya.arthalita.fauzi@gmail.com>
2026-08-29 21:22:10 +0700

    fix: CV Download and YouTube Ref

--- js/projects.js
+++ js/projects.js
@@ -14,7 +14,7 @@ const projectsData = [
         featured: true,
         shortDescription: "A singleplayer game about a man who’s trying to escape a village by finding statues and key while avoiding a supernatural that called Leak.",
         description: "Bli Dadang, an expectant father from a small village who comes home to find his village completely empty. He finds his wife unconscious on the floor, with their unborn child mysteriously gone from her womb. Desperate, he heads to the village head's house near the local Quail for answers, but chased by a Leak along the way. He discovers that the main exit has been closed and locked by the Leak, so he looks for statues to unlock the key’s place in order to open the main gate. As he searches for the key inside the village head's house, every torch suddenly lights up for a warning sign that the Leak has found him.",
-        youtube: "dQw4w9WgXcQ",
+        youtube: "mMOZzwyI6ec",
         itch: "https://vinnettayay.itch.io/le-ak",
         github: "https://github.com/vinnettayay/Le-Ak",
         genre: "Horror",
@@ -45,7 +45,7 @@ const projectsData = [
         featured: false, // Render as large featured project on homepage
         shortDescription: "A singleplayer game where the player takes a role of an operator who fights off malfunction germs to protect their company.",
         description: "The game starts with an operator working at a night shift at BioPurge, a research center company. One night, he detects an experimental germ has mutated and entered his room through the ventilation system that connected to the lab. Afraid of the infection might spread throughout the facility, he immediately sterilizes his room and calls his colleague for help. While waiting, he\’s faced with two choices: \n\n- Wait for his colleague to arrive and play safe \n-Take action to the microscopic world to stop the germs spreading.",
-        youtube: "dQw4w9WgXcQ", // YouTube Video ID
+        youtube: "yt5I6rB4L1w", // YouTube Video ID
         itch: "https://vinnettayay.itch.io/zerospot-opver",
         genre: "FPS",
         platform: "PC / Web",
